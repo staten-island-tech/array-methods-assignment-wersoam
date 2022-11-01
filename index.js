22279740e99a9745abdce92b61b9fe56ab71cdba
wersoam <cowistrash@gmail.com>
2022-11-01 10:15:23 -0400

    hand traps

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const people(){
-
-}
+const handtraps = [
+  "ash blossom and joyous spring",
+  "maxx c",
+  "ghost ogre and snow rabbit",
+  "droll and lock bird",
+  "nibiru the primal being",
+  "psy-framegear gamma",
+  "effect veiler",
+  "dimension shifter",
+  "infinite impermanence",
+  "kuriboh",
+];
+console.log(handtraps);
