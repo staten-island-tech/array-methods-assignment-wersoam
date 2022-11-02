d83d92356ea377ac13c897e85d02b68b6f5ae27b
wersoam <cowistrash@gmail.com>
2022-11-02 10:16:36 -0400

    properties

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const handtraps = [
-  "ash blossom and joyous spring",
-  "maxx c",
-  "ghost ogre and snow rabbit",
-  "droll and lock bird",
-  "nibiru the primal being",
-  "psy-framegear gamma",
-  "effect veiler",
-  "dimension shifter",
-  "infinite impermanence",
-  "kuriboh",
+  {name:"ash blossom and joyous spring", category:[zombie,tuner,fire,drawnegate] }
+ {name: "maxx c",category:[insect,earth,tcgbanned,cardadvantage]}
+  {name:"ghost ogre and snow rabbit",category:[tuner]}
+  {name:"droll and lock bird",category:[drawnegate]}
+  {name:"nibiru the primal being",category:[token]}
+  {name:"psy-framegear gamma",category:[garnet]}
+  {name:"effect veiler",category:[monsnegate]}
+  {name:"dimension shifter",category:[banish,]}
+  {name:"infinite impermanence",category:[trap,]}
+  {name:"kuriboh",category:[dmgnegate]}
 ];
-console.log(handtraps);
+
+handtraps.forEach((interuptions) => {
+  console.log(interuptions);
+});
