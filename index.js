2de7b2670bd5328dbdf03b27ef1df85878a55abf
wersoam <cowistrash@gmail.com>
2022-11-03 10:16:42 -0400

    category finished

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const handtraps = [
-  {name:"ash blossom and joyous spring", category:[zombie,tuner,fire,drawnegate] }
- {name: "maxx c",category:[insect,earth,tcgbanned,cardadvantage]}
-  {name:"ghost ogre and snow rabbit",category:[tuner]}
-  {name:"droll and lock bird",category:[drawnegate]}
-  {name:"nibiru the primal being",category:[token]}
-  {name:"psy-framegear gamma",category:[garnet]}
-  {name:"effect veiler",category:[monsnegate]}
-  {name:"dimension shifter",category:[banish,]}
-  {name:"infinite impermanence",category:[trap,]}
-  {name:"kuriboh",category:[dmgnegate]}
+  {
+    name: "ash blossom and joyous spring",
+    category: ["zombie", "discard", "fire", "drawnegate"],
+  },
+  { name: "maxx c", category: ["insect", "earth", "discard", "cardadvantage"] },
+  {
+    name: "ghost ogre and snow rabbit",
+    category: ["psychic", "discard", "light", "destroy"],
+  },
+  {
+    name: "droll and lock bird",
+    category: ["drawnegate", "discard", "wind", "spellcaster"],
+  },
+  {
+    name: "nibiru the primal being",
+    category: ["token", " tribute", " light", "rock"],
+  },
+  {
+    name: "psy-framegear gamma",
+    category: ["garnet", "psychic", "light", "monsnegate"],
+  },
+  {
+    name: "effect veiler",
+    category: ["monsnegate", "discard", "light", "spellcaster"],
+  },
+  {
+    name: "dimension shifter",
+    category: ["banish", "discard", "spellcaster", "dark"],
+  },
+  {
+    name: "infinite impermanence",
+    category: ["trap", "monsnegate", "lanenegate", "normaltrap"],
+  },
+  { name: "kuriboh", category: ["dmgnegate", "fiend", "dark", "discard"] },
 ];
 
 handtraps.forEach((interuptions) => {
